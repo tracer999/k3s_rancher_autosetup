af69b53e4e2b3b110c5988b2394de3c33595aa79
SH YANG <tracer999@gmail.com>
2025-04-20 22:57:05 +0000

    마스터 인스톨 수정

--- install_k3s_full_stack.sh
+++ install_k3s_full_stack.sh
@@ -63,7 +63,23 @@ if [[ "$mode" == "1" ]]; then
   echo "[11/11] 로컬 Docker Registry 설치"
   if ! command -v docker &> /dev/null; then
     echo "🐳 Docker가 설치되지 않았습니다. 설치 진행..."
-    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    echo "[1] Docker APT 저장소 설정"
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+      sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+      https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    echo "[2] Docker 설치"
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   fi
 
   if ! docker ps --format '{{.Names}}' | grep -q '^registry$'; then
